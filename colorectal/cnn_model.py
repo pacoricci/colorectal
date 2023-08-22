7507283c543cfd02b8ec0c761bbd5315c0c7fe4c
Paco Ricci <paco.ricci@gmail.com>
2023-08-22 18:24:38 +0200

    Changes in project structure

---
+++ colorectal/cnn_model.py
@@ -0,0 +1,29 @@
+from keras import layers, Input, Model, regularizers
+from colorectal import constants, custom_layers
+
+
+def gen_model():
+    _input_shape = Input(shape=[150, 150, 3])
+    _out = layers.GaussianNoise(0.03)(_input_shape)
+    _out = layers.Conv2D(32,
+                         (8, 8),
+                         strides=(4, 4),
+                         padding='valid',
+                         use_bias=False,
+                         kernel_regularizer=regularizers.L2(constants.WEIGHT_DECAY))(_out)
+    _out = layers.BatchNormalization(momentum=constants.BATCH_NORMALIZATION_MOMENTUM)(_out)
+    _out = layers.ReLU()(_out)
+    _out = layers.MaxPooling2D((2, 2))(_out)
+    _out = custom_layers.se_block([18, 18, 32])(_out)
+    _out = custom_layers.cat3([18, 18, 32], 64)(_out)
+    _out = layers.MaxPooling2D((2, 2))(_out)
+    _out = custom_layers.se_block([9, 9, 64])(_out)
+    _out = custom_layers.cat3([9, 9, 64], 128)(_out)
+    _out = layers.GlobalAveragePooling2D()(_out)
+    _out = layers.Dense(32, activation='relu')(_out)
+    _out1 = layers.Dense(8,
+                         activation='softmax',
+                         name='cce',
+                         kernel_regularizer=regularizers.L2(constants.WEIGHT_DECAY))(_out)
+    out2 = custom_layers.Ident(name='ole')(_out)
+    return Model(_input_shape, [_out1, out2])
--- main.py
+++ main.py
@@ -1,36 +1,33 @@
 import tensorflow as tf
 
-import constants
-import dataset
-import cnn_model
-import training
+import colorectal as cl
 
-tf.random.set_seed(constants.TF_SEED)
+tf.random.set_seed(cl.constants.TF_SEED)
 # Dataset download
-ds, ds_info = dataset.dataset_download()
+ds, ds_info = cl.dataset.dataset_download()
 # Normalization of images: image_tensor = image_tensor/255
-ds = dataset.normalize(ds)
+ds = cl.dataset.normalize(ds)
 # Dataset division in training set, validation set, test set
-ds_train, ds_validation, ds_test = dataset.dataset_split(ds)
+ds_train, ds_validation, ds_test = cl.dataset.dataset_split(ds)
 # One Hot Encoding
-ds_train = dataset.one_hot_encoding(ds_train)
-ds_validation = dataset.one_hot_encoding(ds_validation)
-ds_test = dataset.one_hot_encoding(ds_test)
+ds_train = cl.dataset.one_hot_encoding(ds_train)
+ds_validation = cl.dataset.one_hot_encoding(ds_validation)
+ds_test = cl.dataset.one_hot_encoding(ds_test)
 # Dataset augmentation
-ds_train = dataset.rotation(ds_train)
-ds_train = dataset.flip(ds_train)
+ds_train = cl.dataset.rotation(ds_train)
+ds_train = cl.dataset.flip(ds_train)
 # Cache, shuffle, batch, prefetch
-ds_train = dataset.ca_sh_ba_pr(ds_train)
-ds_validation = dataset.ca_sh_ba_pr(ds_validation)
-ds_test = dataset.ca_sh_ba_pr(ds_test)
+ds_train = cl.dataset.ca_sh_ba_pr(ds_train)
+ds_validation = cl.dataset.ca_sh_ba_pr(ds_validation)
+ds_test = cl.dataset.ca_sh_ba_pr(ds_test)
 #
 #
 # Generate the model
-model = cnn_model.gen_model()
+model = cl.cnn_model.gen_model()
 # Model training
-history = training.fit(model, ds_train, ds_validation)
+history = cl.training.fit(model, ds_train, ds_validation)
 # Evaluate the model on test set
-test_loss, test_cce_loss, test_ole_loss, test_accuracy = training.evaluate(model, ds_test)
+test_loss, test_cce_loss, test_ole_loss, test_accuracy = cl.training.evaluate(model, ds_test)
 
 
 
